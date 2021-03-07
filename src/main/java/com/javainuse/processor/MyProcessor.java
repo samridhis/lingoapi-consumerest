5bcad273fa492c960a0e9e0be07e3744bb9cfac3
samridhis <samridhis@gmail.com>
2021-03-07 22:19:31 +1100

    attached processor coverting object collection to string and returning string as response

--- src/main/java/com/javainuse/processor/MyProcessor.java
+++ src/main/java/com/javainuse/processor/MyProcessor.java
@@ -14,7 +14,16 @@ public class MyProcessor implements Processor {
 	public void process(Exchange exchange) throws Exception {
 		var body = exchange.getIn().getBody(String.class);
 		LOGGER.info("my body: {}", body);
-
+		System.out.println("Ahaan is printing in this method");
 		exchange.getIn().setBody("Changed Body");
 	}
 }
+
+/*
+[10:08 pm, 07/03/2021] Ani1: import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jackson.JacksonDataFormat;
+[10:10 pm, 07/03/2021] Ani1: .to("https://samfhirsdf.azurehealthcareapis.com/Practitioner").process(new MyProcessor());
+				.to("https://samfhirsdf.azurehealthcareapis.com/Practitioner").process(new MyProcessor());
+
+* */
